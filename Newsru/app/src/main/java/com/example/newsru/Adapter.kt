12e2f35f181816cf69c8e16fb8428a46b3041fe2
KingMassak <80716208+KingMassak@users.noreply.github.com>
2021-03-17 22:19:58 +0300

    add webView

--- Newsru/app/src/main/java/com/example/newsru/Adapter.kt
+++ Newsru/app/src/main/java/com/example/newsru/Adapter.kt
@@ -3,36 +3,43 @@ package com.example.newsru
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import android.webkit.WebView
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 
+
 class Adapter(val newsList: List<News>):RecyclerView.Adapter<Adapter.NewsViewHolder>() {
-
-
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
             val itemView = LayoutInflater.from(parent.context).inflate(R.layout.news, parent, false)
+            val imageView=itemView.findViewById<ImageButton>(R.id.imageView)
+            val browse=itemView.findViewById<WebView>(R.id.browse)
+            val newsurl=itemView.findViewById<TextView>(R.id.newsTime)
+
+            
+            imageView.setOnClickListener {
+                browse.settings.javaScriptEnabled=true
+            browse.loadUrl(newsurl.text as String)
+           }
+
             return NewsViewHolder(itemView)
         }
-
         override fun getItemCount() = newsList.size
-
         override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
             return holder.bind(newsList[position])
         }
-
         class NewsViewHolder(view:View):RecyclerView.ViewHolder(view){
-        private val image:ImageView=view.findViewById(R.id.imageView)
-        private val title:TextView=view.findViewById(R.id.newsTitle)
-        private val description:TextView=view.findViewById(R.id.newsDescription)
+            private val image:ImageButton=view.findViewById(R.id.imageView)
+            private val title:TextView=view.findViewById(R.id.newsTitle)
+            private val description:TextView=view.findViewById(R.id.newsDescription)
             private val time_url:TextView=view.findViewById(R.id.newsTime)
         fun bind (news:News)
         {
             Picasso.get().load(news.urlToImage).into(image)
             title.text =news.title
             description.text=news.description
-            time_url.text=news.publishedAt!!.replace("T"," ")!!.replace("Z","")+"\n"+news.url
+            time_url.text=news.url
         }
     }
 
--- Newsru/app/src/main/java/com/example/newsru/MainActivity.kt
+++ Newsru/app/src/main/java/com/example/newsru/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.newsru
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
@@ -9,34 +9,53 @@ import retrofit2.Callback
 import retrofit2.Response
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     lateinit var mService: RetrofitService
     lateinit var layoutManager: LinearLayoutManager
-
-
+  private lateinit var  adapter:Adapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.news)
         setContentView(R.layout.activity_main)
 
 
+        val request = Retrofit.buildService(RetrofitService::class.java)
+        val call = request.getDataList()
 
-
-        val request=Retrofit.buildService(RetrofitService::class.java)
-        val call=request.getDataList()
-        call.enqueue(object :Callback<Result>{
+        call.enqueue(object : Callback<Result> {
             override fun onResponse(call: Call<Result>, response: Response<Result>) {
-                if (response.isSuccessful) {
-                    recyclerView.apply {
-                        layoutManager = LinearLayoutManager(this@MainActivity)
-                        adapter = Adapter(response.body()!!.articles)
-                    }
+
+                updateViews(call, response)
+                swipeRefresh!!.setOnRefreshListener {
+                Handler().postDelayed({
+                    swipeRefresh.isRefreshing = false;
+                    updateViews(call, response)
+                }, 1000)
                 }
             }
-            override fun onFailure(call: Call<Result>, t: Throwable) {
-                TODO("Not yet implemented")
+         override fun onFailure(call: Call<Result>, t: Throwable) {
             }
         })
     }
+    fun updateViews(call: Call<Result>, response: Response<Result>) {
+        if(call.isExecuted)
+        {
+            if (response.isSuccessful) {
+                recyclerView.apply {
+                    layoutManager = LinearLayoutManager(this@MainActivity)
+                    adapter = Adapter(response.body()!!.articles)
+                }
+            }
+                else
+                {
+                    Handler().postDelayed({
+                        updateViews(call, response)
+                    },1000)
+                }
 
+            }
+        }
 
 }
+
+
